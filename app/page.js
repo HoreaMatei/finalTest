b92224349d11f559678c671ad4e9f91edeabaf40
HoreaMatei <mateihorea17@yahoo.com>
2024-09-30 12:19:25 +0300

    video settings

--- app/page.js
+++ app/page.js
@@ -39,7 +39,7 @@ function FetchOnClick() {
   }
   return (
     <div className="page">
-      <video autoPlay loop muted className="videoBg">
+      <video controls preload="none" autoPlay loop muted className="videoBg">
         <source src="./fufu3.mp4" type="video/mp4" />
       </video>
 
